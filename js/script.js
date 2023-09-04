574cc605e4039a4ec403849a2e77e84d0bc988a2
iftekharr001 <iftekharr001@gmail.com>
2023-09-04 04:04:29 -0700

    added catagory

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-const handleCatagoris = () =>{
-    fetch("https://openapi.programming-hero.com/api/videos/categories")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-}
-handleCatagoris()
+const handleCatagory = async () => {
+    const response = await fetch(
+      "https://openapi.programming-hero.com/api/videos/categories"
+    );
+    const data = await response.json();
+
+    const tabContainer = document.getElementById('tab-container');
+    const dataContainer = data.data;
+    console.log(dataContainer)
+    dataContainer.forEach((catagory) =>{
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <a onclick="handleTubeLoad('${catagory.category_id}')" class="tab">${catagory.category}</a>
+        `;
+        tabContainer.appendChild(div)
+    });
+};
+const handleTubeLoad = async (catagoryId) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${catagoryId}`
+  );
+  const data = await response.json();
+  console.log(data)
+};
+handleCatagory()
