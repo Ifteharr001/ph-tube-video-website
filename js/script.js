d632c30a0f00ff0b424b92303c57de0aae027d76
iftekharr001 <iftekharr001@gmail.com>
2023-09-04 02:09:01 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const handleCatagoris = () =>{
+    fetch("https://openapi.programming-hero.com/api/videos/categories")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+}
+handleCatagoris()
